2fee0d1ffed7ce075b0f51ff569c4fab21816090
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-06-01 16:12:49 -0300

    fetching pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from "react";
+import type { Pokemon } from './types';
+import PokemonList from "./PokemonList";
+
+async function fetchPokemons(offset: number = 0, limit: number = 20) {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+    )
+
+    if (response.status >= 200 && response.status <= 208 && response.ok) {
+      const data = await response.json()
+
+      const pokemons = data.results.map((
+        raw: any, index: number) => ({
+          ...raw,
+          id: offset + index + 1
+        })
+      )
+
+      return pokemons
+    } else {
+      throw new Error('Erro ao obter os Pokémons');
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 function App() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+  useEffect(
+    () => {
+      fetchPokemons()
+        .then(
+          data => setPokemons(
+            previusData => previusData = data
+          )
+        )
+        .catch(
+          _ => setPokemons(
+            previusData => previusData = []
+          )
+        )
+    }, []
+  );
 
   return (
     <>
-      Hello world!
+      {
+        pokemons ?
+          <PokemonList pokemons={pokemons} /> :
+          <div>vazio</div>
+      }
     </>
   )
 }
